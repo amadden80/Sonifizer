45fc781f3d066ec3cfd0526436c842b6bd9709b5
amadden80 <amadden80@gmail.com>
2013-07-30 09:50:47 -0400

    Sonifizer: The Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sonifizer::Application.config.secret_key_base = '4857e495fcc78dcc2183c24317f7ff36baf07e5677eff96eb1a00db33f5fe35fd7521d1fc12f3da0c84acaaed010e6d8c306a25f34cafaa987c394454e986077'
