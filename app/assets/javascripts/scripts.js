d17c9a4f54ebf8262a20adf43325774650d5d8ca
amadden80 <amadden80@gmail.com>
2013-08-08 11:18:00 -0400

    Add Logo Display Fade

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -3,7 +3,10 @@
 $(function(){
 
     $('.social-buttons').hide();
+
     $("#Sonifizer-Logo").hide();
+    
+    $("#Sonifizer-Logo").attr('src', "http://Sonifizer.com/sonifizer.png");
 
     setTimeout(function(){$('.social-buttons').fadeIn(1000);}, 1000);
     setTimeout(function(){$("#Sonifizer-Logo").fadeIn(1000);}, 1000);
