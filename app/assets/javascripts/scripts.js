dcd12e4710daba4686034ccb29e7197135149bf5
amadden80 <amadden80@gmail.com>
2013-08-08 11:10:22 -0400

    Add Logo

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -3,8 +3,10 @@
 $(function(){
 
     $('.social-buttons').hide();
+    $("#Sonifizer-Logo").hide();
+
     setTimeout(function(){$('.social-buttons').fadeIn(1000);}, 1000);
+    setTimeout(function(){$("#Sonifizer-Logo").fadeIn(1000);}, 1000);
 
 });
-
 
